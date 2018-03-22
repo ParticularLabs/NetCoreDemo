40cb6f6d4f992d25f7b83d4084657d645be75712
indualagarsamy <indu.alagarsamy@particular.net>
2018-03-22 11:40:40 -0700

    adding launchSettings for all the projects

---
+++ src/Marketing.Api/Models/ProductDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Marketing.Api.Models
+{
+    public class ProductDetails
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
