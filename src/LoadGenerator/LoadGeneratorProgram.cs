4aee2d1cb122638dc4d7d27b7fe6a830a3857d2a
indualagarsamy <indu.alagarsamy@particular.net>
2018-04-04 15:05:21 -0700

    clean up LoadGenerator

--- src/LoadGenerator/LoadGeneratorProgram.cs
+++ src/LoadGenerator/LoadGeneratorProgram.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Threading.Tasks;
-    using System.Collections.Generic;
     using EShop.Messages.Commands;
     using ITOps.Shared;
     using NServiceBus;
@@ -12,15 +11,27 @@
         static async Task Main(string[] args)
         {
             Console.Title = "Load Generator";
-
+            
             var endpointConfiguration = new EndpointConfiguration("LoadGenerator");
             endpointConfiguration.ApplyCommonNServiceBusConfiguration();
 
             var endpoint = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
 
+            Console.WriteLine("Press enter to send a message");
+            Console.WriteLine("Press any key to exit");
+
             while (true)
             {
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Sending PlaceOrder message for ProductId = 1");
                 await endpoint.Send(new PlaceOrder { ProductId = 1 })
                     .ConfigureAwait(false);
                 
