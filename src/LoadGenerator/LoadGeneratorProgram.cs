d84641ea9addbf77892025b91ba68bb7c4a29516
Sean Feldman <feldman.sean@gmail.com>
2018-04-04 13:34:34 -0600

    Add initial version of load generator

---
+++ src/LoadGenerator/LoadGeneratorProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EShop.Messages.Commands;
+using ITOps.Shared;
+using NServiceBus;
+
+namespace LoadGenerator
+{
+    using System.Threading.Tasks;
+
+    class LoadGeneratorProgram
+    {
+        static async Task Main(string[] args)
+        {
+            var endpointConfiguration = new EndpointConfiguration("LoadBalancer");
+            endpointConfiguration.ApplyCommonNServiceBusConfiguration();
+
+            var endpoint = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(100))
+                    .ConfigureAwait(false);
+
+                await endpoint.Send(new PlaceOrder {ProductId = 1})
+                    .ConfigureAwait(false);
+            }
+
+            await endpoint.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
